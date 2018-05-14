bc357a151c6632382fba01ab6657055e8498da59
jrmak <jmak@sdsu.edu>
2018-05-14 01:47:05 -0700

    Add files via upload

--- ABM/server.py
+++ ABM/server.py
@@ -1,60 +1,92 @@
 from mesa.visualization.modules import CanvasGrid, ChartModule
-from ModularVisualization import ModularServer
+from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
 
-from agents import Red, Yellow, Green, Black, Monkey
+from agents import Red, Orange, Yellow, Green, Blue, Purple, Black, Gray, Monkey
 from model import Movement
 
 # should match the same width and height as in model.py
-width = 29
-height = 32
+width = 104
+height = 104
 
 def movement_portrayal(agent):
 
     if agent is None:
         return
 
-    portrayal = {"Shape": "rect", "Filled": "true", "w": 0.8, "h": 0.8, "Layer": 0}
+    portrayal = {"Shape": "rect", "Filled": "true", "w": 1, "h": 1, "Layer": 0}
 
     if type(agent) is Red:
         portrayal["Shape"] = "rect"
         portrayal["Color"] = "red"
-        portrayal["r"] = 1
+        portrayal["w"] = 1
+        portrayal["h"] = 1
         portrayal["Layer"] = 0
 
-    elif type(agent) is Green:
+    elif type(agent) is Orange:
         portrayal["Shape"] = "rect"
-        portrayal["Color"] = "green"
-        portrayal["r"] = 1
+        portrayal["Color"] = "orange"
+        portrayal["w"] = 1
+        portrayal["h"] = 1
         portrayal["Layer"] = 0
 
     elif type(agent) is Yellow:
         portrayal["Shape"] = "rect"
         portrayal["Color"] = "yellow"
-        portrayal["r"] = 1
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+        portrayal["Layer"] = 0
+
+    elif type(agent) is Green:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "green"
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+        portrayal["Layer"] = 0
+
+    elif type(agent) is Blue:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "blue"
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+        portrayal["Layer"] = 0
+
+    elif type(agent) is Purple:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "purple"
+        portrayal["w"] = 1
+        portrayal["h"] = 1
         portrayal["Layer"] = 0
 
     elif type(agent) is Black:
         portrayal["Shape"] = "rect"
         portrayal["Color"] = "black"
-        portrayal["r"] = 1
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+        portrayal["Layer"] = 0
+
+    elif type(agent) is Gray:
+        portrayal["Shape"] = "rect"
+        portrayal["Color"] = "gray"
+        portrayal["w"] = 1
+        portrayal["h"] = 1
         portrayal["Layer"] = 0
 
     elif type(agent) is Monkey:
         portrayal["Shape"] = "circle"
-        portrayal["Color"] = "blue"
-        portrayal["r"] = 1
+        portrayal["Color"] = "white"
+        portrayal["r"] = 3
         portrayal["Layer"] = 1
 
     return portrayal
 
-monkey_movement_chart = {"Label": "Golden Monkey", "Color": "purple"}
+# monkey_movement_chart = {"Label": "Golden Monkey", "Color": "purple"}
 
-agent_slider = UserSettableParameter('slider', "Number of Agents", 1, 2, 10, 1)
+agent_slider = UserSettableParameter('slider', "Number of Agents", 10, 10, 30, 1)
 
 canvas = CanvasGrid(movement_portrayal, width, height)
-chart_count = ChartModule([monkey_movement_chart])
+# chart_count = ChartModule([monkey_movement_chart])
 model_params = {"strategy": "switch", "num_monkey": agent_slider}
 
-server = ModularServer(Movement, [canvas, chart_count], "Movement", model_params)
+server = ModularServer(Movement, [canvas], "Movement", model_params) # deleted ', chart_count' after canvvas
 server.launch()
