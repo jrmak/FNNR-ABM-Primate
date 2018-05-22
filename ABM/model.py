4aff3c714734742dd1e27fc1faea5d18275a375d
jrmak <jmak@sdsu.edu>
2018-05-22 04:05:34 -0700

    Add files via upload

--- ABM/model.py
+++ ABM/model.py
@@ -2,13 +2,13 @@ from mesa.model import Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
-import random
-from agents import Red, Orange, Yellow, Green, Blue, Purple, Black, Gray
+from agents import *
 
 masterdict = {'Red':[], 'Orange':[], 'Yellow':[], 'Green':[], 'Blue':[],
               'Purple':[], 'Black':[], 'Gray':[]}  # master dictionary
-# keys: Black, Red, Yellow, Green land classifications, from least to most suitable
-# values: grid coordinates that belong to that land type
+# dictionary keys: land suitability (or elevation, etc.) categorized by color
+# dictionary values: grid coordinates that belong to that land type
+
 
 class Movement(Model):
 
@@ -21,18 +21,26 @@ class Movement(Model):
     number_of_purple = 0
     number_of_gray = 0
 
-    def __init__(self, width=89, height=104, torus=False, seed=42,
-                 strategy=None, num_monkey=10):
+    def __init__(self, width = 89, height = 104, torus = False,
+                 time = 0, number_of_families = 10, number_of_monkeys = 0, monkey_birth_count = 0,
+                 monkey_death_count = 0):
         # seed can be changed
         # torus = False means monkey movement can't 'wrap around' edges
-        super().__init__(seed=seed)
-        self.number_of_monkeys = num_monkey
+        super().__init__()
+        self.time = time
+        self.number_of_families = number_of_families
+        self.number_of_monkeys = number_of_monkeys
+        self.monkey_birth_count = monkey_birth_count
+        self.monkey_death_count = monkey_death_count
 
         self.grid = MultiGrid(width, height, torus)
 
         self.schedule = RandomActivation(self)
-        data = {"Monkey": lambda m: m.number_of_monkeys}
-        self.datacollector = DataCollector(data)
+
+        self.datacollector = DataCollector({"Monkey": lambda m: m.number_of_monkeys})
+        self.datacollector2 = DataCollector({"Monkey": lambda m: m.monkey_birth_count})
+        self.datacollector3 = DataCollector({"Monkey": lambda m: m.monkey_death_count})
+        self.datacollector4 = DataCollector({"Monkey": lambda m: demographic_structure_list})
 
         # generate land
         gridlist = self._readASCII('aggregated_dem.txt')
@@ -46,21 +54,88 @@ class Movement(Model):
         self._populate(gridlist, Black)
         self._populate(gridlist, Gray)
 
-        # create monkey agents
+        """
+        # create monkey agents - early version, each pixel = 1 monkey, no families
         for i in range(self.number_of_monkeys):
             # pos = (random.randint(0,88), random.randint(0,103))
             superlist = masterdict['Red'] + masterdict['Orange'] + masterdict['Yellow'] + masterdict['Green'] \
-                                                 + masterdict['Blue'] + masterdict['Purple'] + masterdict['Black']
+                                                 + masterdict['Blue'] + masterdict['Purple']
             pos = random.choice(superlist)
             from agents import Monkey
             monkey = Monkey(i, self)
             self.grid.place_agent(monkey, pos)
             self.schedule.add(monkey)
+        """
+
+        # create monkey agents - each pixel represents a family group of 25-45 monkeys
+
+        superlist = masterdict['Red'] + masterdict['Orange'] + masterdict['Yellow'] + masterdict['Green'] \
+                    + masterdict['Blue'] + masterdict['Purple']
+
+        if self.time == 0:  # only do this on the first step
+            for i in range(self.number_of_families):
+                pos = random.choice(superlist)
+                from agents import Family
+                family_size = random.randint(25, 45)
+                family_id = i
+                family = Family(family_id, self, pos, family_size)
+                self.grid.place_agent(family, pos)
+                self.schedule.add(family)
+
+                from agents import Monkey
+                for monkey_id in range(family_size):
+                    gender = random.randint(0, 1)
+                    if gender == 1:
+                        last_birth_interval = random.randint(0, 3)
+                    else:
+                        last_birth_interval = 0
+                    mother = 0  # no parent check for first generation
+                    choice = random.random()  # 0 - 1 float
+                    if choice <= 0.11:
+                        age = random.uniform(0, 1)
+                        age_category = 0
+                        demographic_structure_list[0] += 1
+                    elif 0.11 < choice <= 0.27:
+                        age = random.uniform(1, 3)
+                        age_category = 1
+                        demographic_structure_list[1] += 1
+                    elif 0.27 < choice <= 0.42:
+                        age = random.uniform(3, 7)
+                        age_category = 2
+                        demographic_structure_list[2] += 1
+                    elif 0.42 < choice <= 0.62:
+                        age = random.uniform(7, 10)
+                        age_category = 3
+                        demographic_structure_list[3] += 1
+                    elif 0.62 < choice <= 0.96:
+                        age = random.uniform(10, 25)
+                        age_category = 4
+                        demographic_structure_list[4] += 1
+                        # starting representation of male defection
+                        structure_convert = random.random()
+                        if structure_convert > 0.25:
+                            gender = 1
+                    elif 0.96 < choice:
+                        age = random.uniform(25, 30)
+                        age_category = 5
+                        demographic_structure_list[5] += 1
+                        gender = 1
+
+                    monkey = Monkey(monkey_id, self, pos, family_size, gender, age, age_category, family_id,
+                                    last_birth_interval, mother)
+
+                    self.number_of_monkeys += 1
+                    self.schedule.add(monkey)
+
 
     def step(self):
         # necessary; tells model to move forward
+        self.time += (1/73)
         self.schedule.step()
         self.datacollector.collect(self)
+        self.datacollector2.collect(self)
+        self.datacollector3.collect(self)
+        self.datacollector4.collect(self)
 
     def _readASCII(self, text):
         # reads in a text file that determines the environmental grid setup
@@ -72,73 +147,30 @@ class Movement(Model):
         cells = []  # list of cities
         for line in abody:
             cells.append(line.split(" "))
-        # print(cells)
         return cells
 
     def _populate(self, grid, land_type):
-        #places land tiles on the grid
+        # places land tiles on the grid
         prefix = "number_of_{}"
-        counter = 0
-        while counter < land_type.seed * (self.grid.width * self.grid.height):
-            #pos = self.grid.find_empty()
-            # generate land randomly for now, but later, import Maxent output to grid
-            for y in range(104):
-                for x in range(89):
-                    elev = int(round(float(grid[y][x])))  # .index()
-                    pos = x, y
-                    land = land_type(counter, self)
-                    if land_type.min_elev < elev < land_type.max_elev:
-                        self.grid.place_agent(land, pos)
-                        self.schedule.add(land)
-                        land_name = land_type.__name__.lower()
-                        attr_name = prefix.format(land_name)
-                        masterdict[land.__class__.__name__].append(pos)
-                        val = getattr(self, attr_name)
-                        val += 1
-                        setattr(self, attr_name, val)
-                        counter += 1
-
-# matplotlib inline
+        counter = 0  # sets agent ID
+        for y in range(104):
+            for x in range(89):
+                elev = int(round(float(grid[y][x])))  # .index()
+                pos = x, y
+                land = land_type(counter, self)
+                if land_type.min_elev < elev < land_type.max_elev:
+                    self.grid.place_agent(land, pos)
+                    self.schedule.add(land)
+                    land_name = land_type.__name__.lower()
+                    attr_name = prefix.format(land_name)
+                    masterdict[land.__class__.__name__].append(pos)
+                    val = getattr(self, attr_name)
+                    val += 1
+                    setattr(self, attr_name, val)
+                    counter += 1
 
 
-import pandas as pd
-from model import Movement
-"""
-import matplotlib.pyplot as plt
-
-from tqdm import tqdm_notebook
-
-
-def show(result, strategy):
-
-    with plt.style.context("fivethirtyeight"):
-        ax = result.plot(kind="line", figsize=(8,5))
-        ax.set_xlabel("Run")
-        ax.set_ylabel("Population size")
-        ax.set_title("Evolution of monkey population with '{}' strategy".format(strategy))
-
-# as the model develops, different strategies, or scenarios, will be used
-"""
-
-time = 250
-def run(time, **kwargs):
-
-    result = {"Monkey": []}
-    model = Movement(**kwargs)
-
-    # progress = tqdm_notebook(total = time)
-    for t in range(time):
-
-        result["Monkey"].append(model.number_of_monkeys)
-        model.step()
-
-        # progress.update()
-
-    # progress.close()
-
-    return pd.DataFrame(result)
-
-strategy = "switch"
-run(time, strategy = strategy)
-
-# show(result, strategy)
+model = Movement()
+time = 3
+for t in range(time):
+    model.step()
