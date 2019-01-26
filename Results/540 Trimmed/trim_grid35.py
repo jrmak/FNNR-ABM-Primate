13f1cb611297e6de7beb445a884d60cf0f2ef693
jrmak <jmak@sdsu.edu>
2019-01-26 00:32:38 -0800

    Add files via upload

---
+++ Results/540 Trimmed/trim_grid35.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+"""
+Deletes coordinates from a csv file if its y value is below 65 (for an 85x100 spatial grid,
+this creates an 85x35 segment of the top portion).
+"""
+
+
+#text = 'testing.csv'
+def readCSV(file, number):
+    f = open(file + str(number) + '.csv', 'r+')
+    body = f.readlines()
+    abody = body[2:]  # ignore header
+    f.seek(0) # reset pointer
+    for line in abody: # format ('57,42,62,73')
+        if len(line) == 7:
+            if int(line[4:6]) >= 65:
+                f.write(line)
+        else:
+            if len(line.split(",")[0]) == 1 and line.split(",")[0] != 'x' and \
+                    line.split(",")[0] != '\n' and int(line.split(",")[0]) < 11 :
+                if int(line[3:5]) >= 65:
+                    f.write(line)
+    f.truncate()
+    f.close()
+
+for numb in range(1,31):
+    text = 'abm_export_density_plot_with_humans_'
+    print('Progress: ' + str(numb) + ' / 30')
+    readCSV(text, numb)
+
+# for number in range(1, 31):
+#     readCSV(text, number)
