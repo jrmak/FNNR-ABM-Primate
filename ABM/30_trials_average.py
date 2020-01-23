7893f8ed04dd20399057bc511dd85eb85be3bf8c
jrmak <25870074+jrmak@users.noreply.github.com>
2020-01-22 17:22:27 -0800

    Update 30_trials_average.py

--- ABM/30_trials_average.py
+++ ABM/30_trials_average.py
@@ -2,17 +2,9 @@ import os
 import time
 
 """
-This script takes model file outputs and creates an 'average' run out of those files, for use in creating a summary Excel graph
+This script takes certain file outputs and creates an 'average' run out of those files, for use in creating a summary Excel graph
 for scientific purposes. For educational or observational purposes, you can skip straight to creating an Excel graph from any single
-model run's file output; this script is optional.
-
-After you run the model some X number of trials per setting, put all of the trials for each setting in one folder.
-Create subfolders for each file type (all human demographic output files in one folder, etc.).
-Then run this script from the same directory as the other .csv files.
-
-This code assumes 3 sets of 30 trials and 1460 steps (20 years). It will take each of the 30 trials' outputs and average them.
-In other words, the output of this script looks like a new model run, but its step values are just the average of the input files' values.
-The number of rows should be adjusted from 245 to however many that your output files have. 245 is the default for a 20-year model run.
+model run's file output; this script is not useful in most cases.
 """
 
 set_file_name = 'average_human_population.csv'
